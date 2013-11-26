e6e1316e854377680f93f530ebc96df02b8bc063
Michael Hauser-Raspe (Arch) <michael.hauser-raspe@cantab.net>
2013-11-26 01:07:18 +0000

    created initial demo repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForemExample::Application.config.secret_key_base = 'b894d7cfc8e51ac5f828e8e1ea8f9ff08f68ba9944643a655af147160601ba0d867301ab93c43a9427589181101ca8e775a139304f01ae74c683e9a750fe8141'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ForemExample::Application.config.session_store :cookie_store, key: '_forem-example_session'
